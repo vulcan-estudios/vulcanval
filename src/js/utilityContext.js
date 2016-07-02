95b306a4aa0e5e220ba53448144eef80da5378e9
Romel Pérez <ronelprhone@gmail.com>
2016-07-01 22:51:13 -0500

    update: utility context

--- src/js/utilityContext.js
+++ src/js/utilityContext.js
@@ -12,39 +12,12 @@
 const utilityContext = {
 
   /**
-   * This is only available on the client side, on server this will be `null`.
-   *
-   * Reference to the form element used in jQuery plugin instance.
-   *
-   * @type {external:jQuery}
-   * @default null
-   */
-  $form: null,
-
-  /**
-   * This is only available on the client side, on server this will be `null`.
-   *
-   * Reference to the field used in the specific validation in task.
-   *
-   * @type {external:jQuery}
-   * @default null
-   */
-  $field: null,
-
-  /**
    * Reference to the {@link https://www.npmjs.com/package/validator validator}
    * package.
    *
    * @type {Object}
    */
   validator: null,
-
-  /**
-   * Reference to the settings used in the validation.
-   *
-   * @type {settings}
-   */
-  settings: null,
 
   /**
    * A method used to get another field value in the data {@link map} used in
