554f8f92b6d65431feedf997b035905f303a06b2
Romel Pérez <ronelprhone@gmail.com>
2016-07-01 22:57:07 -0500

    docs: plugin change

--- src/js/plugin/_change.js
+++ src/js/plugin/_change.js
@@ -1,12 +1,20 @@
 const ui = require('./_ui');
 
-const change = function (settings, field, e) {
+/**
+ * On field change.
+ *
+ * @param  {settings} settings
+ * @param  {fieldSettings} field
+ * @param  {Object} ev - jQuery event.
+ */
+const change = function (settings, field, ev) {
   'use strict';
 
   const invalid = field.$el.vulcanval('inspect', field.name);
   const wasInvalid = field.$el.data('vv-valid') === false;
   const lastMsg = field.$el.data('vv-msg');
 
+  // Field general.
   field.$el.data({
     'vv-modified': true,
     'vv-msg': invalid
