07421b91a388fe5855476ada8897cd8d905242db
Romel Pérez <ronelprhone@gmail.com>
2016-07-03 01:33:51 -0500

    test: client validate

--- test/client/validate/index.js
+++ test/client/validate/index.js
@@ -1,18 +1,97 @@
+fixture.setBase('test/client/validate');
+$(fixture.load('forms.html')).appendTo('body');
+
+
 describe('Validate', function () {
 
-  it('Invalid fields', function () {
-    //
+  const $form = $('#validate1');
+  const $input1 = $form.find('[name=name]');
+  const $input2 = $form.find('[name=email]');
+  $form.vulcanval({
+    fields: [{
+      name: 'name',
+      validators: {
+        isLength: { min: 4 }
+      }
+    }, {
+      name: 'email',
+      required: true,
+      validators: {
+        isEmail: true
+      }
+    }]
   });
 
-  it('Valid fields', function () {
-    //
+  describe('Validate fields', function () {
+
+    it('Validate fields (invalid)', function () {
+      $form.vulcanval('validate');
+    });
+
+    it('Invalid fields (class)', function () {
+      assert.isTrue($form.hasClass('vv-form_error'));
+      assert.isFalse($input1.hasClass('vv-field_error'));
+      assert.isTrue($input2.hasClass('vv-field_error'));
+    });
+
+    it('Invalid fields (state)', function () {
+      assert.isFalse($form.data('vv-valid'));
+      assert.isTrue($input1.data('vv-valid'));
+      assert.isFalse($input2.data('vv-valid'));
+    });
+
+    it('Validate fields (valid)', function () {
+      $input2.val('mail@mail.com');
+      $form.vulcanval('validate');
+    });
+
+    it('Valid fields (class)', function () {
+      assert.isFalse($form.hasClass('vv-form_error'));
+      assert.isFalse($input1.hasClass('vv-field_error'));
+      assert.isFalse($input2.hasClass('vv-field_error'));
+    });
+
+    it('Valid fields (state)', function () {
+      assert.isTrue($form.data('vv-valid'));
+      assert.isTrue($input1.data('vv-valid'));
+      assert.isTrue($input2.data('vv-valid'));
+    });
   });
 
-  it('Invalid field', function () {
-    //
-  });
+  describe('Validate one field', function () {
 
-  it('Valid field', function () {
-    //
+    it('Validate field (invalid)', function () {
+      $input2.val('invalid');
+      $form.vulcanval('validate', 'email');
+    });
+
+    it('Invalid field (class)', function () {
+      assert.isTrue($form.hasClass('vv-form_error'));
+      assert.isFalse($input1.hasClass('vv-field_error'));
+      assert.isTrue($input2.hasClass('vv-field_error'));
+    });
+
+    it('Invalid field (state)', function () {
+      assert.isFalse($form.data('vv-valid'));
+      assert.isTrue($input1.data('vv-valid'));
+      assert.isFalse($input2.data('vv-valid'));
+    });
+
+    it('Validate field (valid)', function () {
+      $input2.val('mail@mail.com');
+      $form.vulcanval('validate', 'email');
+    });
+
+    it('Valid field (class)', function () {
+      assert.isFalse($form.hasClass('vv-form_error'));
+      assert.isFalse($input1.hasClass('vv-field_error'));
+      assert.isFalse($input2.hasClass('vv-field_error'));
+    });
+
+    it('Valid field (state)', function () {
+      assert.isTrue($form.data('vv-valid'));
+      assert.isTrue($input1.data('vv-valid'));
+      assert.isTrue($input2.data('vv-valid'));
+    });
   });
 });
