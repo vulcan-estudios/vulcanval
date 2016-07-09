95ae93e862cff2c195a9300e2dc91c8dbcf13cd7
Romel Pérez <ronelprhone@gmail.com>
2016-07-09 03:46:14 -0500

    update: utils format

--- src/js/utils.js
+++ src/js/utils.js
@@ -58,14 +58,51 @@ const utils = {
     }
   },
 
+  removeArrayDuplicates (arr) {
+    'use strict';
+
+    const obj = {};
+    for (let i = 0; i < arr.length; i++) {
+      obj[arr[i]] = true;
+    }
+
+    arr = [];
+    for (let key in obj) {
+      arr.push(key);
+    }
+
+    return arr;
+  },
+
   format (str, params) {
+    'use strict';
 
     str = String(str);
     params = params || {};
 
-    for (var p in params) {
-      if (params.hasOwnProperty(p)) {
-        str = str.replace(new RegExp(`\{\{${p}\}\}`, 'g'), params[p]);
+    const walk = (obj, list, i) => {
+      if (!list[i]) return;
+      if (typeof obj[list[i]] === 'object') {
+        return walk(obj[list[i]], list, i+1);
+      } else {
+        return obj[list[i]];
+      }
+    };
+
+    let name, value;
+    let props = str.match(/\{\{\w+(\w|\.\w+)*\}\}/g);
+
+    if (props && props.length) {
+
+      props = utils.removeArrayDuplicates(props);
+      props = props.map(p => p.replace('{{', '').replace('}}', '').split('.'));
+
+      for (var i=0; i<props.length; i++) {
+        value = walk(params, props[i], 0);
+        if (value) {
+          name = props[i].join('.');
+          str = str.replace(new RegExp(`{{${name}}}`, 'g'), value);
+        }
       }
     }
 
