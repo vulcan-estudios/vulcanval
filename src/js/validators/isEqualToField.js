111d7bd7dd54fa62341795a719f4f404b89fea1b
Romel Pérez <ronelprhone@gmail.com>
2016-07-09 16:35:41 -0500

    update: custom validators

---
+++ src/js/validators/isEqualToField.js
@@ -0,0 +1,12 @@
+/**
+ * @name isEqualToField
+ * @memberof settings.validators
+ * @function
+ *
+ * @description
+ * This field value has to be the same as another field the form. The name of
+ * the comparing field should be set as a string parameter.
+ */
+module.exports = function isEqualToField (value, field) {
+  return value === this.get(field);
+};
