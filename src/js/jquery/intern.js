0738e02ec5312792e2812b0193409cc165ba2b47
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-01 18:32:41 +0800

    Delete src directory

--- src/js/jquery/intern.js
+++
@@ -1,8 +0,0 @@
-const jquery = require('jquery');
-const external = require('../external');
-
-// Replace local jQuery module with the node module jQuery.
-external.jQuery = jquery;
-
-// Require the library.
-require('./index');
--- src/js/log.js
+++
@@ -1,7 +0,0 @@
-const Log = require('prhone-log');
-
-module.exports = new Log('VulcanVal', {
-  history: false,
-  scale: 2,
-  throwErrors: true
-});
